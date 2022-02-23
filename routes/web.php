1c2197c35cf792d8879a027c02e8e5dba81ac148
RobertoVillani <robertovillani58@gmail.com>
2022-02-23 16:22:28 +0100

    stampa post con vue

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 //Area Pubblica - Frontoffice
 Route::get('/', function () {
-    return view('welcome');
+    return view('front');
 });
 Auth::routes();
 
